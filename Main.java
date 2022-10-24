77e1de211c4535fe5a9564343f9ca600b046ad07
Szymon Borzdyński <106021186+Szymon982@users.noreply.github.com>
2022-10-24 08:06:37 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,38 +1,53 @@
 package com.company;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 
 public class Main {
-    static ArrayList<String> Lista_imion = new ArrayList<String>();
-    static ArrayList<ArrayList<String>> Lista_imiona_na_litere = new ArrayList<ArrayList<String>>();
-    static ArrayList<ArrayList<imie>> Lista_imiona_na_litere_2 = new ArrayList<ArrayList<imie>>();
+    static ArrayList<String> Lista_imion = new ArrayList<>();
+    static ArrayList<String> Lista_imion_bez_duplikatow = new ArrayList<>();
+    static ArrayList<String> Duplikaty = new ArrayList<>();
+    static ArrayList<ArrayList<imie>> Lista_koncowa = new ArrayList<>();
     static String st;
-
+    static String pulpit = System.getProperty("user.home") + "/Desktop";
     public static void main(String[] args) throws IOException {
         System.out.println("Autor: Szymon Borzdyński");
 
-        ArrayList<String> lista1 = Odczytaj_plik("C:\\Users\\Dom\\Desktop\\pliki do JP\\Imiona1.txt");
-        ArrayList<String> lista2 = Odczytaj_plik("C:\\Users\\Dom\\Desktop\\pliki do JP\\Imiona2.txt");
-        ArrayList<String> lista3 = Odczytaj_plik("C:\\Users\\Dom\\Desktop\\pliki do JP\\Imiona3.txt");
+
+        ArrayList<String> lista1 = Odczytaj_plik(pulpit + "\\pliki do JP\\Imiona1.txt");
+        ArrayList<String> lista2 = Odczytaj_plik(pulpit + "\\pliki do JP\\Imiona2.txt");
+        ArrayList<String> lista3 = Odczytaj_plik(pulpit + "\\pliki do JP\\Imiona3.txt");
 
         Lista_imion.addAll(lista1);
         Lista_imion.addAll(lista2);
         Lista_imion.addAll(lista3);
 
         Collections.sort(Lista_imion);
-        System.out.println(Lista_imion);
         for(int i=0;i<26;i++){
-            Lista_imiona_na_litere.add(new ArrayList<>());
+            Lista_koncowa.add(new ArrayList<>());
         }
-        //System.out.println(numer_litery("A"));
-        zliczanie(Lista_imion);
+
+        usun_duplikaty(Lista_imion,Lista_imion_bez_duplikatow);
+        zlicz_wystapienia(Lista_imion);
+        wypisz_ile_wystapien("Wiktor");
+        wypisz_ile_wystapien("Ewa");
+        System.out.println("LISTA: ");
+        System.out.print("[ ");
+        for(int i = 0; i< Lista_koncowa.size(); i++){
+            System.out.print("[ ");
+            for (int j = 0; j< Lista_koncowa.get(i).size(); j++){
+                System.out.print(Lista_koncowa.get(i).get(j).imie);
+                System.out.print(",");
+            }
+            System.out.print(" ]");
+        }
+        System.out.print(" ]");
+
         System.exit(69);
     }
 
     public static ArrayList<String> Odczytaj_plik(String dir) throws IOException {
-        ArrayList<String> Lista = new ArrayList<String>();
+        ArrayList<String> Lista = new ArrayList<>();
         File plik = new File(dir);
         BufferedReader buf = new BufferedReader(new FileReader(plik));
         while ((st = buf.readLine()) != null){
@@ -40,6 +55,7 @@ public class Main {
         }
         return Lista;
     }
+
     public static int numer_litery(String litera) {
         char[] ch = litera.toCharArray();
         int numer = 0;
@@ -52,12 +68,42 @@ public class Main {
         return numer;
 
     }
-    public static void zliczanie(ArrayList<String> lista_imion){
-        for(int i=0; i<lista_imion.size();i++){
-            Lista_imiona_na_litere.get(numer_litery(lista_imion.get(i).substring(0, 1))).add(lista_imion.get(i));
+
+    public static void usun_duplikaty(ArrayList<String> lista,ArrayList<String> lista_nowa) {
+        for(int i=0;i<lista.size();i++) {
+            if(!lista_nowa.contains(lista.get(i))){
+                lista_nowa.add(lista.get(i));
+            } else {
+                Duplikaty.add(lista.get(i));
+            }
         }
-        System.out.println(Lista_imiona_na_litere);
     }
 
+    public static void zlicz_wystapienia(ArrayList<String> lista_imion){
+        try {
+            int licznik = 0;
+            for(int i=0; i<Lista_imion.size();i++){
+                if (licznik != 0 && Duplikaty.contains(lista_imion.get(i))){
+                    licznik = 0;
+                    continue;
+                }
+                int liczba_wystapien = Collections.frequency(Lista_imion,lista_imion.get(i));
+                Lista_koncowa.get(numer_litery(lista_imion.get(i).substring(0, 1))).add(new imie(lista_imion.get(i),liczba_wystapien));
+                licznik++;
+            }
+        }
+        catch(Exception InvalidParameterException) {
+            System.out.println("Niepoprawny parametr");
+        }
+    }
 
+    public static void wypisz_ile_wystapien(String imie){
+        for(int i=0; i<Lista_koncowa.size();i++){
+            for(int j=0; j<Lista_koncowa.get(i).size();j++){
+                if(Objects.equals(Lista_koncowa.get(i).get(j).imie, imie)){
+                    System.out.println("Liczba wystapien imienia "+imie+": "+Lista_koncowa.get(i).get(j).liczba_wystapien);
+                }
+            }
+        }
+    }
 }
